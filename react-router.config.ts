67f1f8c02c158783c90364ef16cd7728ba26bbff
Lucas Cipriano <lucasc.lima26@gmail.com>
2026-05-06 09:12:37 -0300

    Update router configuration to disable SSR and prerender exam pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,16 @@
 import type { Config } from "@react-router/dev/config";
+import assert from "node:assert";
+import { readdir } from "node:fs/promises";
+import { resolve } from "node:path";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  async prerender() {
+    const files = await readdir(resolve(import.meta.dirname, "../data/exams"))
+    assert(files.every((file) => file.startsWith("exam-") && file.endsWith(".json")));
+    const range = Array.from({ length: files.length }, (_, i) => i + 1);
+    return ["/", ...range.map((i) => `/exam/${i}`)];
+  }
 } satisfies Config;
