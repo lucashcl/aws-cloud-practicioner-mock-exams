69b1b070fb5c8817f06c40864b1970e1b69c434a
Lucas Cipriano <lucasc.lima26@gmail.com>
2026-05-06 09:31:39 -0300

    Add data scraping step to GitHub Actions workflow and fix path in prerender function

--- react-router.config.ts
+++ react-router.config.ts
@@ -8,7 +8,7 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
   async prerender() {
-    const files = await readdir(resolve(import.meta.dirname, "../data/exams"))
+    const files = await readdir(resolve(import.meta.dirname, "data/exams"))
     assert(files.every((file) => file.startsWith("exam-") && file.endsWith(".json")));
     const range = Array.from({ length: files.length }, (_, i) => i + 1);
     return ["/", ...range.map((i) => `/exam/${i}`)];
